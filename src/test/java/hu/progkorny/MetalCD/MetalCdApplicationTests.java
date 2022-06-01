35a45712e5fbc9fc7395c6ef492801140c2b7676
Kulcsar Laszlo Attila <kulcsarlaszloattila@gmail.com>
2022-06-01 18:24:53 +0200

    modositas5

---
+++ src/test/java/hu/progkorny/MetalCD/MetalCdApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.progkorny.MetalCD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetalCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
